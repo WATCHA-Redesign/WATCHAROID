aeefbdec1b6e6d521024c0573749bd862245e68c
todayiswindy <dpswpfkgy@gmail.com>
2021-05-21 17:04:56 +0900

    [FEAT] TopSearchContentView, YourMovieMateView

--- app/src/main/java/com/yjooooo/watcharoid/ui/home/view/HomeFragment.kt
+++ app/src/main/java/com/yjooooo/watcharoid/ui/home/view/HomeFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.observe
 import com.google.android.material.tabs.TabLayoutMediator
 import com.yjooooo.watcharoid.R
 import com.yjooooo.watcharoid.databinding.FragmentHomeBinding
--- app/src/main/java/com/yjooooo/watcharoid/ui/search/viewmodel/SearchViewModel.kt
+++ app/src/main/java/com/yjooooo/watcharoid/ui/search/viewmodel/SearchViewModel.kt
@@ -5,16 +5,32 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.yjooooo.watcharoid.R
 import com.yjooooo.watcharoid.ui.search.model.HighScoreData
+import com.yjooooo.watcharoid.ui.search.model.TopSearchData
 
 class SearchViewModel: ViewModel() {
     private val _highScoreList = MutableLiveData<List<HighScoreData>>()
     val highScoreList: LiveData<List<HighScoreData>>
         get() = _highScoreList
 
+    private val _topSearchContentList = MutableLiveData<List<TopSearchData>>()
+    val topSearchContentList: LiveData<List<TopSearchData>>
+        get() = _topSearchContentList
+
+
     fun setHighScoreList() {
         _highScoreList.value = mutableListOf(
             HighScoreData(R.drawable.img_rv_high_score_1),
             HighScoreData(R.drawable.img_rv_high_score_2)
+        )
+    }
+
+    fun setTopSearchContentList() {
+        _topSearchContentList.value = mutableListOf(
+            TopSearchData(R.drawable.card_search_small_android1),
+            TopSearchData(R.drawable.card_search_small_android2),
+            TopSearchData(R.drawable.card_search_small_android3),
+            TopSearchData(R.drawable.card_search_small_android4),
+            TopSearchData(R.drawable.card_search_small_android5)
         )
     }
 }
