4050e277d46ae0101d4ad78434b20c5003648411
Yunjoo Son <thsduswn4141@naver.com>
2021-05-29 20:31:38 +0900

    #25 [FEAT] Network : RetrofitBuilder 생성, Service 인터페이스 생성

---
+++ app/src/main/java/com/yjooooo/watcharoid/network/api/SearchService.kt
@@ -0,0 +1,18 @@
+package com.yjooooo.watcharoid.network.api
+
+import retrofit2.http.GET
+
+interface SearchService {
+    @GET("/searchpage/popular")
+    fun getPopularSearch(){
+
+    }
+    @GET("/searchpage/collection")
+    fun getWatchaCollection(){
+
+    }
+    @GET("searchpage/mate")
+    fun getMovieMate(){
+
+    }
+}
