456f7e8b388cfaae892691f84621a916a07fcf86
Yunjoo Son <thsduswn4141@naver.com>
2021-05-29 23:16:45 +0900

    Modify SearchYjooService.kt

--- app/src/main/java/com/yjooooo/watcharoid/network/api/SearchYjooService.kt
+++ app/src/main/java/com/yjooooo/watcharoid/network/api/SearchYjooService.kt
@@ -1,6 +1,6 @@
 package com.yjooooo.watcharoid.network.api
 
-import com.yjooooo.watcharoid.ui.home.model.ResponseHighScoreList
+import com.yjooooo.watcharoid.ui.search.model.ResponseHighScoreList
 import retrofit2.http.GET
 
 interface SearchYjooService {
